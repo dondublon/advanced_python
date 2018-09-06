d806ec84147acc6771c8211f0823f40a6d586d8b
Yaroslav Benin <yaroslav_benin@epam.com>
2018-09-06 11:30:50 +0300

    Task 5 - Timer.

--- task5_timer.py
+++ task5_timer.py
@@ -1,9 +1,21 @@
 from threading import Timer
+import time
+
+n = 100
+tick = 0.1
+
+# I use "pseudo-threads" - Timer that invokes another timer in his working function.
 
 
-def hello():
-    print("hello, world")
+def print_num(name, number):
+    print("pseudo-thread name:", name, number)
+    if number+1 < n:
+        t = Timer(tick * 2, function=print_num, args=(name, number+2))
+        t.start()
 
 
-t = Timer(5, hello)
-t.start()
+t1 = Timer(0, print_num, args=("1", 0))
+t2 = Timer(0, print_num, args=("1", 1))
+t1.start()
+time.sleep(tick)
+t2.start()
