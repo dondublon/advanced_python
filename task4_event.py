e64314d415bc152faebfa5a24c52107cb26c794f
Yaroslav Benin <yaroslav_benin@epam.com>
2018-09-06 11:12:14 +0300

    "Event" almost ready.

--- task4_event.py
+++ task4_event.py
@@ -1,22 +1,39 @@
 import threading
 import time
 
+n = 10
+
 
 def foo(ev):
     print('Foo', end='')
     ev.set()
 
 
-def bar(ev):
-    while not ev.is_set():
-        print('Waiting ...')
-        ev.wait(1)
-    print('Bar')
+class MyThread(threading.Thread):
+    def __init__(self, name, ev, starting):
+        super().__init__()
+        self.name = name
+        self.current = starting
+        self.ev = ev
+
+    def run(self):
+        while self.current <= n:
+            if not self.ev.is_set():
+                # print('Waiting ...')
+                self.ev.wait(1)
+            self.ev.clear()
+            print(self.name, self.current)
+            self.current += 2
+            time.sleep(0.1)
+            self.ev.set()
 
 
-print_event = threading.Event()
-ft = threading.Thread(name='foo', target=foo, args=(print_event,))
-bt = threading.Thread(name='bar', target=bar, args=(print_event,))
-bt.start()
-time.sleep(3)
-ft.start()
+my_event = threading.Event()
+t1 = MyThread("thread-1", my_event, 0)
+t2 = MyThread("thread-2", my_event, 1)
+t1.start()
+time.sleep(0.1)
+t2.start()
+
+t1.join()
+t2.join()
